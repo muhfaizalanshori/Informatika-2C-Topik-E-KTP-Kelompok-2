db5cd46ed2732ef11fa53d5c51d1156d02ef2c41
Septianrio <septianrio44@gmail.com>
2017-05-19 15:27:15 +0700

    Update Penduduk.java

--- src/ektpkelompok2/Penduduk.java
+++ src/ektpkelompok2/Penduduk.java
@@ -1,19 +1,53 @@
 package ektpkelompok2;
-
-import.java.util.Warga;
 /**
  *
  * @author FAISTAR
  */
-public class Penduduk {
+    public class Warga implements Comparable<Warga> {
+private String nik;
+private String nama;
+private String ttl;
+private String jeniskelamin;
+private String goldarah;
+private String alamat;
+private String agama;
+private String stsperkawinan;
+private String pekerjaan;
+private String kewarganegaraan;
+private String berlaku;
+
+public Warga(String nikWarga, String namaWarga, String ttlWarga){
+setNik(nikWarga);
+setNama(namaWarga);
+setTtl(ttlWarga);
+}
+
+public void setNik(String nikWarga){
+    this.nik = nikWarga;
+}
+public String getNik(){
+return nik;
+}
+public void setNama(String namaWarga) {
+this.nama = namaWarga;
+}
+public String getNama() {
+return nama;
+}
+public void setTtl(String ttlWarga) {
+this.ttl = ttlWarga;
+}
+public String getTtl() {
+return ttl;
+}
+//selanjutnya belum
+
+
+@Override
+public int compareTo(Warga w) {
+return 1;
+}
+
+}
     
-    public static void main(String[] args) {
-Warga<Penduduk> dau  = new Warga<>();
-dau.add(new Penduduk("Java","Agus"));
-dau.add(new Penduduk("C++","Bagus"));
-//Metode 1
-for(Penduduk b:dau){
-System.out.println(w.getNama());
-}
-}
-}
+
