f68f6a5d3f8270858b2783bf95e49d3e83f25519
Muh. Faizal Anshori <muhfaizalanshori@gmail.com>
2017-05-19 17:01:18 +0700

    Update Penduduk.java

--- src/ektpkelompok2/Penduduk.java
+++ src/ektpkelompok2/Penduduk.java
@@ -11,23 +11,30 @@ public class Penduduk {
         ArrayList <Rt> rt = new ArrayList<Rt>();
         
       
-        wrg.add(new Warga("001", "Muh. Faizal Anshori", "Tulungagung, 21 April 1997"));
-        wrg.add(new Warga("002", "Septian Rio", "Tulungagung, 2 juni 1997"));
+        wrg.add(new Warga("001", "Muh. Faizal Anshori", "Tulungagung, 21 April 1997", "L", "A", "Ngunut", "Islam", "Belum Kawin", "Mahasiswa", "WNI", "Seumur Hidup"));
+        wrg.add(new Warga("002", "Septian Rio",         "Tulungagung, 2 juni 1997" ,  "L", "A", "Ngunut", "Islam", "Belum Kawin", "Mahasiswa", "WNI", "Seumur Hidup"));
         wrg.add(new Warga("003", "Falianda", "Tanggerang, 8 Agustus 1998"));
         wrg.add(new Warga("004", "M. yusuf Iwang", "Lamongan, 29 Januari 1998"));
         wrg.add(new Warga("005", "Malik Fajar", "Manado, 9 juni 1997"));
        
-        rt.add(new Rt (wrg.get(3)));
+       /* rt.add(new Rt (wrg.get(3)));
         
         for (Rt r: rt ){
             System.out.println(r.getWarga().getNama()+"\n");
-            
+         */   
             
         
         for(Warga w: wrg ){
             System.out.print(w.getNik()+"\t");
             System.out.print(w.getNama()+"\t");
-            System.out.println(w.getTtl());
+            System.out.print(w.getTtl()+"\t");
+            System.out.print(w.getjenis()+"\t");
+            System.out.print(w.getAlamat()+"\t");
+            System.out.print(w.getAgama()+"\t");
+            System.out.print(w.getstsPerkawinan()+"\t");
+            System.out.print(w.getPekerjaan()+"\t");
+            System.out.print(w.getKewarganegaraan()+"\t");
+            System.out.println(w.getberlaku());
             
             
         /*    
