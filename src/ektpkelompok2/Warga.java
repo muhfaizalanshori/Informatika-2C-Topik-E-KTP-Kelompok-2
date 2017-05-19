87c78951f2ace0dc58442612c620b77ced6769e7
faliandaPw <faliandapw@gmail.com>
2017-05-19 19:06:03 +0700

    Update Warga.java

--- src/ektpkelompok2/Warga.java
+++ src/ektpkelompok2/Warga.java
@@ -1,102 +1,120 @@
 package ektpkelompok2;
 
-    public class Warga implements Comparable<Warga> {
-private String nik;
-private String nama;
-private String ttl;
-private String jeniskelamin;
-private String goldarah;
-private String alamat;
-private String agama;
-private String stsperkawinan;
-private String pekerjaan;
-private String kewarganegaraan;
-private String berlaku;
+public class Warga implements Comparable<Warga> {
+    private String nik;
+    private String nama;
+    private String ttl;
+    private String jeniskelamin;
+    private String goldarah;
+    private String alamat;
+    private String agama;
+    private String stsperkawinan;
+    private String pekerjaan;
+    private String kewarganegaraan;
+    private String berlaku;
         
 public Warga(String nikWarga, String namaWarga, String ttlWarga, String jenisWarga, String goldarahWarga, String alamatWarga, String agamaWarga, String stsperkawinanWarga, String pekerjaanWarga, String kewarganegaraanWarga, String berlakuWarga){
-setNik(nikWarga);
-setNama(namaWarga);
-setTtl(ttlWarga);
-setJenis(jenisWarga);
-setgolDarah(goldarahWarga);
-setAlamat(alamatWarga);
-setAgama(agamaWarga);
-setstsPerkawinan(stsperkawinanWarga);
-setPekerjaan(pekerjaanWarga);
-setKewarganegaraan(kewarganegaraanWarga);
-setBerlaku(berlakuWarga);
+    setNik(nikWarga);
+    setNama(namaWarga);
+    setTtl(ttlWarga);
+    setJenis(jenisWarga);
+    setgolDarah(goldarahWarga);
+    setAlamat(alamatWarga);
+    setAgama(agamaWarga);
+    setstsPerkawinan(stsperkawinanWarga);
+    setPekerjaan(pekerjaanWarga);
+    setKewarganegaraan(kewarganegaraanWarga);
+    setBerlaku(berlakuWarga);
 }    
 
 
 public void setNik(String nikWarga){
     this.nik = nikWarga;
 }
+    
 public String getNik(){
-return nik;
+    return nik;
 }
+    
 public void setNama(String namaWarga) {
-this.nama = namaWarga;
+    this.nama = namaWarga;
 }
+    
 public String getNama() {
-return nama;
+    return nama;
 }
+    
 public void setTtl(String ttlWarga) {
-this.ttl = ttlWarga;
+    this.ttl = ttlWarga;
 }
+    
 public String getTtl() {
-return ttl;
+    return ttl;
 }
 
 public void setJenis(String jenisWarga) {
-this.jenis = jenisWarga;
+    this.jenis = jenisWarga;
 }
+    
 public String getjenis() {
-return jenis;
+    return jenis;
 }
+    
 public void setgolDarah(String goldarahWarga) {
-this.goldarah = goldarahWarga;
+    this.goldarah = goldarahWarga;
 }
+    
 public String getgolDarah() {
-return goldarah;
+    return goldarah;
 }
+    
 public void setAlamat(String alamatWarga) {
-this.alamat = alamatWarga;
+    this.alamat = alamatWarga;
 }
+    
 public String getAlamat() {
-return alamat;
+    return alamat;
 }
+    
 public void setAgama(String agamaWarga) {
-this.agama = agamaWarga;
+    this.agama = agamaWarga;
 }
+    
 public String getAgama() {
-return agama;
+    return agama;
 }
+    
 public void setstsPerkawinan(String stsperkawinanWarga) {
-this.stsperkawinan = stsperkawinanWarga;
+    this.stsperkawinan = stsperkawinanWarga;
 }
+    
 public String getstsPerkawinan() {
-return stsperkawinan;
+    return stsperkawinan;
 }
+    
 public void setPekerjaan(String pekerjaanWarga) {
-this.pekerjaan = pekerjaanWarga;
+    this.pekerjaan = pekerjaanWarga;
 }
+    
 public String getPekerjaan() {
-return pekerjaan;
+    return pekerjaan;
 }
+    
 public void setKewarganegaraan(String kewarganegaraanWarga) {
-this.kewarganegaraan = kewarganegaraanWarga;
+    this.kewarganegaraan = kewarganegaraanWarga;
 }
+    
 public String getKewarganegaraan() {
-return kewarganegaraan;
+    return kewarganegaraan;
 }
+    
 public void setBerlaku(String berlakuWarga) {
-this.berlaku = berlakuWarga;
+    this.berlaku = berlakuWarga;
 }
+    
 public String getberlaku() {
-return berlaku;
+    return berlaku;
 }        
-
-
 
 @Override
 public int compareTo(Warga w) {
