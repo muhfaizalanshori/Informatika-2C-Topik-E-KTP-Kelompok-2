35fca3ee17a93876188b953126beb815345ee6ce
Muh. Faizal Anshori <muhfaizalanshori@gmail.com>
2017-05-20 12:24:28 +0700

    Update HashMapPendudukDriver.java

--- src/ektpkelompok2/HashMapPendudukDriver.java
+++ src/ektpkelompok2/HashMapPendudukDriver.java
@@ -9,15 +9,5 @@ public class HashMapPendudukDriver {
  
  
  
- /*   
-        public static void main(String[] args) {
-HashMap<String, Warga> dau = new HashMap<>();
-dau.put("best seller", new Warga("Java", "Agus"));
-dau.put("new", new Warga("C++", "Bagus"));
-//metode 1
-for(warga a : dau.values()){
-System.out.println(a.getNama()); //tanpa cetak key
-}
-  */ 
   }
 }
